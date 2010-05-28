2c90301f48641d948ca2c104fee0b298cd77f701
Elyézer Mendes Rezende <elyezermr@gmail.com>
2010-05-28 10:43:01 -0300

    Added javascript code to separate the front page's recipe languages list in four columns. Fixed the rating javascript code to don't do the job if don't have any recipe.

--- souschef/media/js/rating.js
+++ souschef/media/js/rating.js
@@ -1,26 +1,29 @@
 jQuery(function($){
     var $rating_items = $('.rating li');
-    var $rating_list = $rating_items.parent();
-    var rating = /rating-(\d)/.exec($rating_list.attr('class'))[1] - 1;
+    
+    if ($rating_items.length > 0) {
+        var $rating_list = $rating_items.parent();
+        var rating = /rating-(\d)/.exec($rating_list.attr('class'))[1] - 1;
 
-    function fill_stars (number) {
-        $($rating_items[number]).prevAll().andSelf().addClass('rating-active');
-    }
+        function fill_stars (number) {
+            $($rating_items[number]).prevAll().andSelf().addClass('rating-active');
+        }
 
-    fill_stars(rating);
+        fill_stars(rating);
 
-    if (!$rating_list.hasClass('rating-readonly')) {
-        $rating_items.each(function(){
-            $(this).hover(function(){
-                $(this).prevAll().andSelf().addClass('rating-hover');
-            }, function(){
-                $(this).prevAll().andSelf().removeClass('rating-hover');
+        if (!$rating_list.hasClass('rating-readonly')) {
+            $rating_items.each(function(){
+                $(this).hover(function(){
+                    $(this).prevAll().andSelf().addClass('rating-hover');
+                }, function(){
+                    $(this).prevAll().andSelf().removeClass('rating-hover');
+                });
             });
-        });
-    } else {
-        $rating_items.addClass('rating-readonly');
-        $rating_items.click(function(e){
-            e.preventDefault();
-        });
-    }
+        } else {
+            $rating_items.addClass('rating-readonly');
+            $rating_items.click(function(e){
+                e.preventDefault();
+            });
+        }
+    };
 });
