b4a8a18bcb417f98562aae73e03afa833b74056b
Og Maciel <omaciel@rpath.com>
2011-02-28 20:03:23 -0500

    Moved some settings to the sample file.

--- production_settings_sample.py
+++ production_settings_sample.py
@@ -13,3 +13,11 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+SITE_HOST = '127.0.0.1:8000'
+DEFAULT_FROM_EMAIL = \
+    'Conary Recipes <contact@conaryrecipes.com>'
+EMAIL_HOST = 'mail.example.com'
+EMAIL_PORT = ''
+EMAIL_HOST_USER = 'contact@example.com'
+EMAIL_HOST_PASSWORD = ''
